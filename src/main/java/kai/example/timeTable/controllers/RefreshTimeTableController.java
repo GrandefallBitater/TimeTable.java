1e1b56879e7667c6387a67b73dece9d1238a5e8c
руслан <ruslan@fattakhov.net>
2024-02-26 04:12:55 +0300

    допилено окно изменения расписания

--- src/main/java/kai/example/timeTable/controllers/RefreshTimeTableController.java
+++ src/main/java/kai/example/timeTable/controllers/RefreshTimeTableController.java
@@ -3,10 +3,25 @@ package kai.example.timeTable.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 @Controller
 public class RefreshTimeTableController {
     @GetMapping(value = "/refresh")
     public String searchRefreshView(Model model) {
         return "chooseGroup";
+    }
+
+    @RequestMapping(value = "/refreshSession")
+    public String RefreshSession(Model model,
+                                 @RequestParam("lesson-name") String name,
+                                 @RequestParam("time") String time,
+                                 @RequestParam("teacher") String teacher,
+                                 @RequestParam("type") String type,
+                                 @RequestParam("audienceNumber") String audienceNumber) {
+        //тут вызов проверок на кореектность введённых данных и возвращение расписания для обновления на странице (сейчас не работает)
+        return "refreshTimeTable";
     }
 }
--- src/main/resources/static/refreshTimeTableScript.js
+++ src/main/resources/static/refreshTimeTableScript.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
 
+    $('.refreshButton').click( function () {
+        let lesson = $(this).parent().parent();
 
+        $('#lesson-name').val(lesson.children('#nameTimeTable').children().text());
+        $('#time').val(lesson.children('#TimeTimeTable').children().text());
+        $('#audienceNumber').val(lesson.children('#ANumTimeTable').children().text());
+        $('#teacher').val(lesson.children('#TeacherTimeTable').children().text());
+        $('#type').val(lesson.children('#typeTimeTable').children().text());
 
+        $('#exampleModal').modal("show");
+    });
+
+    $('.close').click( function () {
+        $('#exampleModal').modal("hide");
+    });
+
+    $('#close').click( function () {
+        $('#exampleModal').modal("hide");
+    });
+
+    $('#submit').click( function () {
+        $('#refreshForm').submit();
+    });
 });
